b7369686c62e60e7a549793069eab7a91686f32f
Herbertkartel <herbzkartel2@gmail.com>
2025-06-17 12:40:38 +0300

    updating database

--- app/firebase.js
+++ app/firebase.js
@@ -7,14 +7,15 @@ import { collection, addDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqAoIYIxZnx8XmIL7OGEUbBgdxWDxhB78",
-  authDomain: "expenses-tracker-bfade.firebaseapp.com",
-  projectId: "expenses-tracker-bfade",
-  storageBucket: "expenses-tracker-bfade.appspot.com",
-  messagingSenderId: "17745216753",
-  appId: "1:17745216753:web:3e67b9e73b9d952c4c9550"
+  apiKey: "AIzaSyAPGQkvxtiLmp9zWvGJWC_RKftnKHgUfN4",
+  authDomain: "expenses-tracker-28670.firebaseapp.com",
+  projectId: "expenses-tracker-28670",
+  storageBucket: "expenses-tracker-28670.firebasestorage.app",
+  messagingSenderId: "210671733588",
+  appId: "1:210671733588:web:437ae957e6d8217016d6c1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
